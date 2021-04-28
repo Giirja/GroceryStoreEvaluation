47f1da2a33edba1fadee88a0d49f4edbdfce1eb2
Giirja <83252004+Giirja@users.noreply.github.com>
2021-04-28 07:29:40 -0400

    Added Interface file for GlobalRepository

---
+++ Repository/IGlobalRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroceryStoreAPI.Model;
+namespace GroceryStoreAPI.Repository
+{
+    public interface IGlobalRepository
+    {
+        public int UpdateCustomerInformation(int id, string name);
+
+        public int AddCustomerDetails(int id, string name);
+
+        public List<Customer> RetrieveaCustomer(int id);
+
+        public List<Customer> DeleteCustomerDetails(int id);
+    }
+}
