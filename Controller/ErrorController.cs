bbd4e1013dbcf98b2fa1db69bd2310493724741b
Giirja <83252004+Giirja@users.noreply.github.com>
2021-04-28 07:26:01 -0400

    Added ErrorController.cs

---
+++ Controller/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroceryStoreAPI
+{
+    public class ErrorController : Controller
+    {
+
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ExceptionPath = exceptionDetails.Path;
+            ViewBag.ExceptionMessage = exceptionDetails.Error.Message;
+            ViewBag.StackTrace = exceptionDetails.Error.StackTrace;
+            return View("Error");
+        }
+    }
+}
